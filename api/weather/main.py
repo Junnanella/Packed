ebb0e28ecb73fe1911b335f82e159f11326719f8
jinspins <uncleben096@gmail.com>
2022-07-12 18:57:42 -0500

    updated structure of returned data from weather api and implemented 2 unit tests

--- api/weather/main.py
+++ api/weather/main.py
@@ -1,81 +1,62 @@
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import requests
 from pydantic import BaseModel
 import datetime
 import os
+
 app = FastAPI()
 
 WEATHER_API_KEY = os.environ["WEATHER_API_KEY"]
 base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
 rest_of_path = f"?unitGroup=us&elements=name%2Ctempmax%2Ctempmin%2Ctemp&include=days%2Ccurrent&key={WEATHER_API_KEY}&contentType=json"
-# months = {
-#     {1:"Jan"},
-#     {2:"Feb"},
-#     {3:"Mar"},
-#     {4:"Apr"},
-#     {5:"May"},
-#     {6:"Jun"},
-#     {7:"July"},
-#     {8:"Aug"},
-#     {9:"Sep"},
-#     {10:"Oct"},
-#     {11:"Nov"},
-#     {12:"Dec"},
-# }
+
 
 class TempOut(BaseModel):
-    temp: int
+    date: dict
 
 
 class TempsOut(BaseModel):
     temps: list[TempOut]
 
 
-def get_date_list(today):
-    dates = []
-    month_back = today.month - 1
-    day = today.day if today.day < 29 else 28
-    year_back = today.year
-    while month_back != today.month:
-        if month_back < 1:
-            month_back = 12
-            year_back -= 1
-        two_digit_month = str(month_back) if month_back >= 10 else f"0{month_back}"
-        dates.append(f"{year_back}-{two_digit_month}-{day}")
-        month_back -= 1
-    print(dates)
-    return dates
+class WeatherQueries:
+    def get_date_list(self, today:datetime):
+        dates = []
+        month_back = today.month - 1
+        day = today.day if today.day < 29 else 28
+        year_back = today.year
+        while month_back != today.month:
+            if month_back < 1:
+                month_back = 12
+                year_back -= 1
+            two_digit_month = str(month_back) if month_back >= 10 else f"0{month_back}"
+            dates.append(f"{year_back}-{two_digit_month}-{day}")
+            month_back -= 1
+        return dates
+
+    def get_weather(self, full_path):
+        response = requests.get(full_path)
+        data = json.loads(response.content)
+        return data
 
 
-def get_weather(query):
-    response = requests.get(query)
-    data = json.loads(response.content)
-    return data
-
-
-# @app.get("/temperature/current")
-# def current_weather(city:str, country:str):
-#     search_parameters = f"{city}%20{country}/today"
-#     query = base_url + search_parameters + rest_of_path
-#     data = get_weather(query)
-#     max_temp = data["days"][0]["tempmax"]
-#     min_temp = data["days"][0]["tempmin"]
-#     temp = (max_temp + min_temp) / 2
-#     return temp
-
-
-@app.get("/temperature/year")
-def temp_list(city:str, country:str):
+@app.get("/temperature/year", response_model=TempsOut)
+def temp_list(city:str, country:str, query=Depends(WeatherQueries)):
     dates = ["today"]
-    dates += get_date_list(datetime.date.today())
+    dates += query.get_date_list(datetime.date.today())
     temps = []
     for date in dates:
         date_section = "today" if date == "today" else f"{date}/{date}"
         search_parameters = f"{city}%20{country}/{date_section}"
-        query = base_url + search_parameters + rest_of_path
-        print(query)
-        data = get_weather(query)
-        temps.append([{"temp_max": data["days"][0]["tempmax"]}, {"temp_min": data["days"][0]["tempmin"]}])
-        # temps.append(data["days"][0]["temp"])
-    return temps
+        full_path = base_url + search_parameters + rest_of_path
+        data = query.get_weather(full_path)
+        # temps.append({ date:[
+        #     {"temp_max": data["days"][0]["tempmax"]},
+        #     {"temp_min": data["days"][0]["tempmin"]},
+        #     {"temp": data["days"][0]["temp"]},
+        # ]})
+        temps.append(
+            {"date": { date: data["days"][0]["temp"]}}
+        )
+    return {"temps": temps}
---
+++ api/weather/test_main.py
@@ -0,0 +1,63 @@
+from main import app, WeatherQueries
+import datetime
+import json
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+class FakeWeatherQueries:
+    def get_weather(self, full_path):
+        data = {
+            "days": [{"temp": 5},]
+        }
+        return data
+    def get_date_list(self, today):
+        return ["2022-06-12", "2022-05-12",]
+
+
+def test_get_date_list():
+    # ARRANGE
+    test_date = datetime.date(2022, 7, 12)
+    correct_dates = [
+        "2022-06-12",
+        "2022-05-12",
+        "2022-04-12",
+        "2022-03-12",
+        "2022-02-12",
+        "2022-01-12",
+        "2021-12-12",
+        "2021-11-12",
+        "2021-10-12",
+        "2021-09-12",
+        "2021-08-12",
+    ]
+
+    # ACT
+    dates = WeatherQueries.get_date_list(self=[],today=test_date)
+
+    # ASSERT
+    assert dates == correct_dates
+
+
+def test_temp_list():
+    # ARRANGE
+    app.dependency_overrides[WeatherQueries] = FakeWeatherQueries
+    correct_info = {
+        "temps": [
+            {"date": {"today": 5}},
+            {"date": {"2022-06-12": 5}},
+            {"date": {"2022-05-12": 5}},
+        ]
+    }
+
+    # ACT
+    response = client.get("/temperature/year?city=Berlin&country=Germany")
+
+
+    # ASSERT
+    response.status_code == 200
+    content = json.loads(response.content)
+    assert content == correct_info
+
+    # CLEAN UP
+    app.dependency_overrides = {}
