186f6f8c1dd8bafd970606033e59d95329408645
Junella Caringal <junellacaringal@gmail.com>
2022-07-12 15:34:00 -0700

    Added form component to main page. Able to handle state changes.

--- ghi/src/MainPage/MainPage.js
+++ ghi/src/MainPage/MainPage.js
@@ -1,9 +1,13 @@
+import { TripForm } from "./TripForm";
 export default function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
       <h1 className="display-5 fw-bold">packed</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">blurb blurb blurb</p>
+      </div>
+      <div>
+        <TripForm />
       </div>
     </div>
   );
