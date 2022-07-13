f74c402995bb6cd380b738054e66ded45c6c9b9e
karmina <karminabarajas653@yahoo.com>
2022-07-13 12:53:37 -0700

    created function that returns currency exchnage rate

--- api/currency/main.py
+++ api/currency/main.py
@@ -1,9 +1,54 @@
 from fastapi import FastAPI
+import requests
+import json
+# set to today 
 
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+url = "https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=USD&amount=1"
+
+payload = {}
+headers= {
+  "apikey": "BzBwcOYZtoPuGSspEZkc5B6poXqOaS48"
+}
+
+def get_currency_rate(url): 
+
+  payload = {}
+  headers= {
+    "apikey": "BzBwcOYZtoPuGSspEZkc5B6poXqOaS48"
+  }
+
+  response = requests.request("GET", url, headers=headers, data = payload)
+
+
+  status_code = response.status_code
+  result = response.text
+  result_split = str(result).splitlines()[-2]
+  return result_split
+
+@app.get("/api/convert")
+def currency_exchange_rate(origin_country, destination_country):
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={origin_country}&from={destination_country}&amount=1"
+    currency_rate = get_currency_rate(url)  
+    return currency_rate
+
+
+# API call output 
+
+# {
+#     "success": true,
+#     "query": {
+#         "from": "USD",
+#         "to": "EUR",
+#         "amount": 1
+#     },
+#     "info": {
+#         "timestamp": 1657670399,
+#         "rate": 0.997805
+#     },
+#     "date": "2022-07-11",
+#     "result": 0.997805
+# }
 
---
+++ journals/karmina.md
@@ -0,0 +1,7 @@
+# July 12
+
+Today our team reviewed our prior days work and talked about what we needed to do. I was able to test my currency exchange rate API to confirm it worked but now I need to create a function that only returns the from, to, amount, result and date. I am having trouble creating a function to complete this task but I will continue to research Fast APIs and react to resolve this. 
+
+# July 11
+
+Today our team set up docker by creating microservices for each API we need to use and the src code. With the help of SEIRS we were able to get the ports up and running. 
