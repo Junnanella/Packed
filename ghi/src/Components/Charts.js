6e0f21a477de91e750151326abfcd3ea957a75fc
Junella Caringal <junellacaringal@gmail.com>
2022-07-14 21:59:02 -0700

    Able to get chart to render based on fetched weather data.

--- ghi/src/Components/Charts.js
+++ ghi/src/Components/Charts.js
@@ -9,49 +9,38 @@ import {
   CategoryScale,
   //   Title,
 } from "chart.js";
-// import { loadWeatherData } from "./MainApi";
+import { loadWeatherData } from "./MainApi";
 
 ChartJS.register(LineElement, PointElement, LinearScale, CategoryScale);
 
+const useWeatherData = () => {
+  const [weather, setWeather] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const weather_response = await loadWeatherData("Seoul", "South Korea");
+      setWeather(weather_response);
+    }
+    fetchData();
+  }, []);
+
+  return weather;
+};
+
 export default function Chart() {
-  // const [weather, setWeather] = useState([]);
+  const weather = useWeatherData();
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const weather_response = await loadWeatherData("Seoul", "South Korea");
-  //     console.log("weather_response:", weather_response);
-  //     setWeather(weather_response);
-  //   }
-  //   fetchData();
-  // }, []);
+  const date_list = weather.map(({ date }) => date);
+  const temp_list = weather.map(({ temperature }) => temperature);
 
-  // const weather_list = weather.map(({ date }) => date);
-  // const temp_list = weather.map(({ temperature }) => temperature);
-  const dates = [
-    "today",
-    "2022-06-14",
-    "2022-05-14",
-    "2022-04-14",
-    "2022-03-14",
-    "2022-02-14",
-    "2022-01-14",
-    "2021-12-14",
-    "2021-11-14",
-    "2021-10-14",
-    "2021-09-14",
-    "2021-08-14",
-  ];
-
-  const [data] = useState({
-    labels: dates,
+  const data = {
+    labels: date_list,
     datasets: [
       {
         label: "Temperatures",
         backgroundColor: "rgba(194, 116, 161, 0.5)",
         borderColor: "rgb(194, 116, 161)",
-        data: [
-          78, 71.3, 60.6, 55.6, 47.6, 36.8, 22.8, 37.9, 52.2, 67.6, 76.3, 79.2,
-        ],
+        data: temp_list,
       },
       //   {
       //     label: "Flight Costs",
@@ -60,7 +49,7 @@ export default function Chart() {
       //     data: [165, 259, 290, 181, 256, 255, 340, 290, 175, 180, 77, 167],
       //   },
     ],
-  });
+  };
 
   return (
     <div className="offset-3 col-6">
