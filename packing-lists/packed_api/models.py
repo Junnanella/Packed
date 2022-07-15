f24d3a7c2e38d31b55523cf12006196cea49f120
jinspins <uncleben096@gmail.com>
2022-07-14 23:57:40 -0500

    created fake api endpoint for weather. Included urls for django project.

--- packing-lists/packed_api/models.py
+++ packing-lists/packed_api/models.py
@@ -7,3 +7,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# class Item(models.Model):
+#     name = models.CharField(max_length=50)
+#     category = models.ForeignKey("Category", related_name="items", on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.name
