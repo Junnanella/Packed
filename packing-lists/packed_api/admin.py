032d12fc9a152958bd3948c962b7248da3d45819
karminabarajas <karminabarajas653@yahoo.com>
2022-07-15 13:19:02 -0700

    created models and admin for packing-lists

--- packing-lists/packed_api/admin.py
+++ packing-lists/packed_api/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, PackingList, Item, PackingListItem, Condition
 
 # Register your models here.
+
+@admin.register(PackingList)
+class PackingListAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(Condition)
+class ConditionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PackingListItem)
+class PackingListItemAdmin(admin.ModelAdmin):
+    pass
+
+
