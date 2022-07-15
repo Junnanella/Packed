a4e39facaf65c7ca22ad47daa5cf3370fe4b7736
Junella Caringal <junellacaringal@gmail.com>
2022-07-15 11:56:20 -0700

    Rendered second mock chart for Flights data

--- ghi/src/DataCharts/LoadApiData.js
+++ ghi/src/DataCharts/LoadApiData.js
@@ -7,26 +7,38 @@ export const loadWeatherData = async (city, country) => {
 
   return temps;
 
-  const response = await fetch(
-    `http://localhost:8001/api/weather?city=${encodeURIComponent(
-      city
-    )}&country=${encodeURIComponent(country)}`
-  );
+  // use below to connect with actual api
+  // const response = await fetch(
+  //   `http://localhost:8001/api/weather?city=${encodeURIComponent(
+  //     city
+  //   )}&country=${encodeURIComponent(country)}`
+  // );
 
-  if (!response.ok) {
-    console.error(await response.json());
-    throw new Error(`Failed to get weather data -- HTTP ${response.status}`);
-  }
+  // if (!response.ok) {
+  //   console.error(await response.json());
+  //   throw new Error(`Failed to get weather data -- HTTP ${response.status}`);
+  // }
 
-  const responseJson = await response.json();
-  return responseJson.temps;
+  // const responseJson = await response.json();
+  // return responseJson.temps;
 };
 
-export const loadFlightData = async () => {
+export const loadFlightData = async (
+  origin_city,
+  destination_city,
+  departure_date,
+  return_date
+) => {
+  const flights = [
+    { date: "today", cost: 100.0 },
+    { date: "2022-06-14", cost: 150.0 },
+    { date: "2022-05-14", cost: 250.0 },
+  ];
+
+  return flights;
   // const response = await fetch("http://localhost:8002/api/flights");
   // const responseJson = await response.json();
-  // return responseJson;
-  return "flight data";
+  // return responseJson.flights;
 };
 
 export const loadCurrencyData = async () => {
