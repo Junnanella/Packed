fd375d765fc8e8b647250c267cce3bd8307d85da
karminabarajas <karminabarajas653@yahoo.com>
2022-07-17 12:35:59 -0700

    created model encoders

--- packing-lists/packed_api/views.py
+++ packing-lists/packed_api/views.py
@@ -1,5 +1,48 @@
-from django.shortcuts import render
+from black import Mode
+# from django.shortcuts import render
+from common.json import ModelEncoder
 from .models import PackingList, Category, Condition, Item, PackingListItem
 
 # Create your views here.
+class PackingListEncoder(ModelEncoder):
+    model = PackingList
+    properties = [
+        "id",
+        "name",
+        "created",
+        "travel_date",
+        "completed",
+        "location"
+    ]
 
+class CategoryEncoder(ModelEncoder):
+    model = Category
+    properties = [
+        "category_name"
+    ]
+
+class ConditionEncoder(ModelEncoder):
+    model = Condition
+    properties = [
+        "item_condition"
+    ]
+
+class ItemEncoder(ModelEncoder):
+    model = Item
+    properties = [
+        "name",
+        "category",
+        "suggested",
+        "condition",
+        "user_item"
+    ]
+
+class PackingListItemEncoder(ModelEncoder):
+    model = PackingListItem
+    properties = [
+        "item",
+        "owner",
+        "quantity",
+        "packed",
+        "packing_list",
+    ]
