a991e0aef7cdac5ac06c7be75bc90076c4cb7f1d
Junella Caringal <junellacaringal@gmail.com>
2022-07-18 17:01:44 -0700

    Added user input component for adding own items to a packing list.

---
+++ ghi/src/PackingListComponents/UserInputItems.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export const UserItemForm = (props) => {
+  const [userItem, setUserItem] = useState("");
+
+  const onClick = async (event) => {
+    console.log(userItem);
+  };
+
+  const onChangeUserItem = (event) => {
+    setUserItem(() => event.target.value);
+  };
+
+  return (
+    <div className="container">
+      <div className="input-group mb-3">
+        <input
+          onChange={onChangeUserItem}
+          value={userItem}
+          placeholder="Add an Item"
+          required
+          type="text"
+          name="user_item"
+          id="user_item"
+          className="form-control"
+        />
+        <button className="btn" type="button" onClick={onClick}>
+          ✅
+        </button>
+      </div>
+    </div>
+  );
+};
