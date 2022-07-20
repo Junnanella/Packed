56b0e2e6854bb09c2e47f69683c32427ef2c8dbf
Junella Caringal <junellacaringal@gmail.com>
2022-07-20 16:04:52 -0700

    Removed dummy data in locations fetch function. Call to locations api works as intended

--- ghi/src/MainPages/MainApi.js
+++ ghi/src/MainPages/MainApi.js
@@ -1,21 +1,5 @@
 export const loadLocationsData = async () => {
-  //   const response = await fetch("http://localhost:8004/api/locations");
-  //   const responseJson = await response.json();
-  //   console.log(responseJson);
-  //   return responseJson.countries;
-
-  const locations = [
-    {
-      id: 1,
-      currency_code: "EUR",
-      country: "Germany",
-    },
-    {
-      id: 2,
-      currency_code: "USD",
-      country: "United States of America",
-    },
-  ];
-
-  return locations;
+  const response = await fetch("http://localhost:8004/api/locations");
+  const responseJson = await response.json();
+  return responseJson.countries;
 };
