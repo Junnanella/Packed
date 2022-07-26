fe0503d4094b2278f8b683face6fc74c3cd9a216
Junella Caringal <junellacaringal@gmail.com>
2022-07-26 15:40:39 -0700

    Updated currency exchange component to take in user input to recalculate total exchange. Added css.

--- ghi/src/DataCharts/CurrencyInfo.js
+++ ghi/src/DataCharts/CurrencyInfo.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { loadCurrencyData } from "./LoadApiData";
+import "./data.css";
 
 const useCurrencyData = (origin_country, destination_country) => {
   const [currencyRate, setCurrencyRate] = useState("");
@@ -22,11 +23,29 @@ export default function CurrencyInfo(props) {
   const { origin_code, destination_code } = props;
   const currencyRate = useCurrencyData(origin_code, destination_code);
 
+  const [currencyInput, setCurrencyInput] = useState(1);
+  const [exchangeOutput, setExchangeOutput] = useState(0.987);
+
+  const onChangeCurrencyInput = (event) => {
+    setCurrencyInput(() => event.target.value);
+    setExchangeOutput(() => currencyRate * event.target.value);
+  };
+
   return (
     <div className="container-sm offset-1">
       <h3 className="mt-5">Current Exchange Rate</h3>
-      <h4>
-        1 {origin_code} = {currencyRate} {destination_code}
+      <input
+        onChange={onChangeCurrencyInput}
+        value={currencyInput}
+        placeholder="1"
+        required
+        type="text"
+        name="currency_input"
+        id="currency_input"
+        className="currency_input"
+      />
+      <h4 className="currency_output">
+        {origin_code} = {exchangeOutput} {destination_code}
       </h4>
     </div>
   );
