163d9779e7ffb165229d4dbcf560f725c31a0aac
Junella Caringal <junellacaringal@gmail.com>
2022-07-26 22:50:45 -0700

    Added logo to navbar

--- ghi/src/MainPages/Nav.js
+++ ghi/src/MainPages/Nav.js
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
+import "./pages.css";
 
 export default function Nav() {
   return (
     <nav className="navbar navbar-expand-lg ">
       <div className="container-fluid">
         <NavLink className="navbar-brand nav-link" to="/">
+          <img src="./packed_p.png" alt="p" className="packed-logo" />
           packed
         </NavLink>
       </div>
