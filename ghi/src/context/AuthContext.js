b35d434111cdcff752d328b1b727e10da4a084ba
jinspins <uncleben096@gmail.com>
2022-07-30 15:21:56 -0500

    received jwt from backend after login

--- ghi/src/context/AuthContext.js
+++ ghi/src/context/AuthContext.js
@@ -12,16 +12,28 @@ export const AuthProvider = ({children}) => {
     let loginUser = async (e) => {
         e.preventDefault();
         
-        const url = "http://localhost:8005/auth/token"
-        const data = {"username": null, "password": null}
+        const url = "http://localhost:8005/auth/token/"
+        const params = {
+            "username": e.target.username.value,
+            "password": e.target.password.value,
+        }
         const fetchConfig = {
             method: "POST",
-            body: JSON.stringify(data),
+            body: JSON.stringify(params),
             headers: {
                 "Content-Type": "application/json",
-            },
+            }
         }
-        const response = await fetch(fetchConfig)
+        const response = await fetch(url, fetchConfig)
+        const data = await response.json()
+
+        if (response.ok) {
+            console.log(data)
+            // setAuthTokens(data)
+            // setUser(data.access)
+        } else {
+            alert("Something went wrong")
+        }
     }
 
     const contextData = {
