01ea9a2c14539627bb63f1b59be7dd1793801302
Junella Caringal <junellacaringal@gmail.com>
2022-08-01 21:49:18 -0700

    Able to send authtokens and render users packing list titles on the browser page.

--- ghi/src/PackingListComponents/PackingListApi.js
+++ ghi/src/PackingListComponents/PackingListApi.js
@@ -1,5 +1,3 @@
-// loading functions for fetching from the backend
-
 export const loadItemsList = async (condition) => {
   const response = await fetch(
     `http://localhost:8005/api/items/conditions/${condition}/`
@@ -12,4 +10,20 @@ export const loadItemsList = async (condition) => {
 
   const responseJson = await response.json();
   return responseJson;
+};
+
+export const loadPackingLists = async (authTokens) => {
+  const fetchConfig = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + String(authTokens?.access),
+    },
+  };
+  const response = await fetch(
+    `http://localhost:8005/api/packing_lists/`,
+    fetchConfig
+  );
+  const responseJson = await response.json();
+  return responseJson.packing_lists;
 };
--- ghi/src/PackingListComponents/PackingLists.js
+++ ghi/src/PackingListComponents/PackingLists.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { loadPackingLists } from "./PackingListApi";
+import AuthContext from "../context/AuthContext";
 
 export const PackingLists = () => {
-  return <h1>Packing Lists page!!</h1>;
+  const [packingLists, setPackingLists] = useState([]);
+  const { authTokens } = useContext(AuthContext);
+
+  useEffect(() => {
+    async function fetchData() {
+      const packingLists = await loadPackingLists(authTokens);
+      console.log("PackingLists", packingLists);
+      setPackingLists(packingLists);
+    }
+    fetchData();
+  }, []);
+
+  return <p>{packingLists[0].title}</p>;
 };
