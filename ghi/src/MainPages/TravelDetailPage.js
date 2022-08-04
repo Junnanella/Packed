c86df6041564e65db2697328ff4ff94fe75c4b2e
Junella Caringal <junellacaringal@gmail.com>
2022-08-03 20:00:05 -0700

    code cleanup

--- ghi/src/MainPages/TravelDetailPage.js
+++ ghi/src/MainPages/TravelDetailPage.js
@@ -6,7 +6,7 @@ import { UserItemForm } from "../PackingListComponents/UserInputItems";
 import SuggestedItems from "../PackingListComponents/Items";
 import WorkingList from "../PackingListComponents/WorkingList";
 import React, { useState, useContext } from "react";
-import AuthContext from '../context/AuthContext'
+import AuthContext from "../context/AuthContext";
 import "./pages.css";
 
 export default function TravelDetailPage() {
@@ -19,56 +19,63 @@ export default function TravelDetailPage() {
   const departure_date = searchParams.get("departure_date");
   const return_date = searchParams.get("return_date");
   const [items, setItems] = useState([]);
-  let {user, authTokens} = useContext(AuthContext);
-
+  let { user } = useContext(AuthContext);
 
   // Will need to pass the above variable to the corresponding components
   return (
     <div>
       <div className="travel-page">
-      <div className="detail-page-header">
-        <h1>Hi {user?.username}! </h1>
-        <h2 className="detail-page-header-text display-4 fw-normal text-center g-5">
-        Create a packing list for {destination_city}, {destination_country}!
-        </h2>
-        <img src="../travel-bags.png" alt="bags" className="bags" /> 
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col item-column detail-columns">
-            <UserItemForm setItems={setItems} items={items} />
-            <SuggestedItems setItems={setItems} items={items} />
-          </div>
-          <div className="col item-column detail-columns">
-            <WorkingList setItems={setItems} items={items}
-              destination_city={destination_city}
-              destination_country={destination_country}
-              departure_date={departure_date}
-              return_date={return_date}
-              origin_country={origin_country}
-            />
-          </div>
-          <div className="col data-column detail-columns">
-            <div className="row">
-              <WeatherChart
+        <div className="detail-page-header">
+          <h1>Hi {user?.username}! </h1>
+          <h2 className="detail-page-header-text display-4 fw-normal text-center g-5">
+            Create a packing list for {destination_city}, {destination_country}!
+          </h2>
+          <img src="../travel-bags.png" alt="bags" className="bags" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col item-column detail-columns">
+              <UserItemForm setItems={setItems} items={items} />
+              <SuggestedItems setItems={setItems} items={items} />
+            </div>
+            <div className="col item-column detail-columns">
+              <WorkingList
+                setItems={setItems}
+                items={items}
                 destination_city={destination_city}
                 destination_country={destination_country}
                 departure_date={departure_date}
                 return_date={return_date}
+                origin_country={origin_country}
               />
             </div>
-            <div className="row currency-data">
-              <CurrencyInfo
-                origin_code={origin_code}
-                destination_code={destination_code}
-                detailPage={false}
-              />
+            <div className="col data-column detail-columns">
+              <div className="row">
+                <WeatherChart
+                  destination_city={destination_city}
+                  destination_country={destination_country}
+                  departure_date={departure_date}
+                  return_date={return_date}
+                />
+              </div>
+              <div className="row currency-data">
+                <CurrencyInfo
+                  origin_code={origin_code}
+                  destination_code={destination_code}
+                  detailPage={false}
+                />
+              </div>
+            </div>
+            <div>
+              <img
+                src="../travel-suitcases.png"
+                alt="suitcases"
+                className="suitcases"
+              />{" "}
             </div>
           </div>
-          <div><img src="../travel-suitcases.png" alt="suitcases" className="suitcases" /> </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
