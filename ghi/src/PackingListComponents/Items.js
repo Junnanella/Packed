4f0e42ef47eda9a4bb160fa9d4c3d0677655b330
Junella Caringal <junellacaringal@gmail.com>
2022-08-04 12:15:28 -0700

    Added classnames to component headers to apply css to.

--- ghi/src/PackingListComponents/Items.js
+++ ghi/src/PackingListComponents/Items.js
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { loadItemsList } from "./PackingListApi";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 import AuthContext from "../context/AuthContext";
 import { useContext } from "react";
 
-export default function SuggestedItems({setItems, items}) {
+export default function SuggestedItems({ setItems, items }) {
   const [conditionalItems, setConditionalItems] = useState([]);
   const [generalItems, setGeneralItems] = useState([]);
-  let {authTokens} = useContext(AuthContext)
+  let { authTokens } = useContext(AuthContext);
   const fetchConfig = {
     method: "GET",
     headers: {
-        "Content-Type": "application/json",
-    }
+      "Content-Type": "application/json",
+    },
   };
   if (authTokens) {
-    fetchConfig.headers.Authorization = "Bearer " + String(authTokens?.access)
+    fetchConfig.headers.Authorization = "Bearer " + String(authTokens?.access);
   }
-  
 
   useEffect(() => {
     async function fetchData() {
       const response = await loadItemsList("cold", fetchConfig);
-      const conditional = response.conditional_items.concat(response.user_favorite_items);
+      const conditional = response.conditional_items.concat(
+        response.user_favorite_items
+      );
       const general = response.general_items;
       setConditionalItems(conditional);
       setGeneralItems(general);
@@ -33,29 +34,34 @@ export default function SuggestedItems({setItems, items}) {
   // console.log("conditional:", conditionalItems)
   // console.log("general:", generalItems)
 
-
   function validate() {
-    const tempItems = [...items]
-    for (let i = 0; i < tempItems.length; i ++) {
-      let item = tempItems[i]
+    const tempItems = [...items];
+    for (let i = 0; i < tempItems.length; i++) {
+      let item = tempItems[i];
       for (let generalItem of generalItems) {
         if (item.name.toLowerCase() === generalItem.name.toLowerCase()) {
           setGeneralItems(
-            generalItems.filter((generalItem) => generalItem.name.toLowerCase() !== item.name.toLowerCase())
+            generalItems.filter(
+              (generalItem) =>
+                generalItem.name.toLowerCase() !== item.name.toLowerCase()
+            )
           );
-          item.suggested = true
-          item.id = generalItem.id
-          setItems(tempItems)
+          item.suggested = true;
+          item.id = generalItem.id;
+          setItems(tempItems);
         }
       }
       for (let conditionalItem of conditionalItems) {
         if (item.name.toLowerCase() === conditionalItem.name.toLowerCase()) {
           setConditionalItems(
-            conditionalItems.filter((conditionalItem) => conditionalItem.name.toLowerCase() !== item.name.toLowerCase())
+            conditionalItems.filter(
+              (conditionalItem) =>
+                conditionalItem.name.toLowerCase() !== item.name.toLowerCase()
+            )
           );
-          item.suggested = true
-          item.id = conditionalItem.id
-          setItems(tempItems)
+          item.suggested = true;
+          item.id = conditionalItem.id;
+          setItems(tempItems);
         }
       }
     }
@@ -78,7 +84,7 @@ export default function SuggestedItems({setItems, items}) {
 
   return (
     <div className="container">
-      <h4>Things you might need</h4>
+      <h4 className="items-heading">Things you might need</h4>
       <div className="input-group mb-3">
         <div>
           <table className="table table-hover">
@@ -93,34 +99,44 @@ export default function SuggestedItems({setItems, items}) {
                   <tr key={item.name}>
                     <td>{item.name}</td>
                     <td>
-                      <button className="btn btn-sm btn-outline-danger" onClick={(e) => addGItem(item)}><FontAwesomeIcon icon={faPlusSquare} /></button>
+                      <button
+                        className="btn btn-sm btn-outline-danger"
+                        onClick={(e) => addGItem(item)}
+                      >
+                        <FontAwesomeIcon icon={faPlusSquare} />
+                      </button>
                     </td>
                   </tr>
                 );
               })}
             </tbody>
           </table>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">Recommended For You</th>
-            </tr>
-          </thead>
-          <tbody>
-            {conditionalItems.map((item) => {
-              return (
-                <tr key={item.name}>
-                  <td>{item.name}</td>
-                  <td>
-                    <button className="btn btn-sm btn-outline-danger" onClick={(e) => addCItem(item)}><FontAwesomeIcon icon={faPlusSquare} /></button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Recommended For You</th>
+              </tr>
+            </thead>
+            <tbody>
+              {conditionalItems.map((item) => {
+                return (
+                  <tr key={item.name}>
+                    <td>{item.name}</td>
+                    <td>
+                      <button
+                        className="btn btn-sm btn-outline-danger"
+                        onClick={(e) => addCItem(item)}
+                      >
+                        <FontAwesomeIcon icon={faPlusSquare} />
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
