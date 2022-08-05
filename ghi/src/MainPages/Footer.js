700f8ad1440ea325480701212e8b9a503913ea7f
william <accrazor@gmail.com>
2022-08-05 15:56:27 -0600

    updated names location on footer

--- ghi/src/MainPages/Footer.js
+++ ghi/src/MainPages/Footer.js
@@ -3,34 +3,25 @@ import React from "react"
 
 export default function Footer() {
     return(        
-        <footer className="py-5 bg-light fixed">
-            {/* <div className="col-md-2 mb-md-0 mb-2 m-3">
-                <h5 className="text-uppercase">About Us</h5>
-                <ul className="list-group flex-md-row">
-                    <li><a href="https://www.linkedin.com/in/josiah-pederson/">Josiah</a></li>
-                    <li><a href="https://www.linkedin.com/in/junella-caringal/">Junella</a></li>
-                    <li><a href="https://www.linkedin.com/in/karminabarajas/">Karmina</a></li>
-                    <li><a href="https://www.linkedin.com/in/william-carter-3b05b7237/">William</a></li>
-                </ul>
-            </div> */}
-
-    <div className="footer-copyright text-center py-3">© 2022 packed:
-        <a href="https://packed-module-3.gitlab.io/packed/"> packed.io</a>
-    </div>
+    <footer className="py-5 bg-light fixed">
+    <h1 className="nav justify-content-center"> About Us </h1>
     <ul className="nav justify-content-center">
-  <li className="nav-item">
-    <a className="nav-link active" href="https://www.linkedin.com/in/josiah-pederson/">Josiah</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link" href="https://www.linkedin.com/in/junella-caringal/">Junella</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link" href="https://www.linkedin.com/in/karminabarajas/">Karmina</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link" href="https://www.linkedin.com/in/william-carter-3b05b7237/">William</a>
-  </li>
-</ul>
+        <li className="nav-item">
+            <a className="nav-link active" href="https://www.linkedin.com/in/josiah-pederson/">Josiah</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="https://www.linkedin.com/in/junella-caringal/">Junella</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="https://www.linkedin.com/in/karminabarajas/">Karmina</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="https://www.linkedin.com/in/william-carter-3b05b7237/">William</a>
+        </li>
+    </ul>
+        <div className="footer-copyright text-center py-3">© 2022 packed:
+            <a href="https://packed-module-3.gitlab.io/packed/"> packed.io</a>
+        </div>
     </footer>
     );
 }
