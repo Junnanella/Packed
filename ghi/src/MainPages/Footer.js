4ed14c9fed0661e2acfbcb6a0555902eeef8350f
william <accrazor@gmail.com>
2022-08-05 16:51:12 -0600

    changed to open links in new tab

--- ghi/src/MainPages/Footer.js
+++ ghi/src/MainPages/Footer.js
@@ -7,16 +7,16 @@ export default function Footer() {
     <h1 className="nav justify-content-center"> About Us </h1>
     <ul className="nav justify-content-center">
         <li className="nav-item">
-            <a className="nav-link active" href="https://www.linkedin.com/in/josiah-pederson/">Josiah</a>
+            <a className="nav-link" href="https://www.linkedin.com/in/josiah-pederson/"target="_blank">Josiah</a>
         </li>
         <li className="nav-item">
-            <a className="nav-link" href="https://www.linkedin.com/in/junella-caringal/">Junella</a>
+            <a className="nav-link" href="https://www.linkedin.com/in/junella-caringal/"target="_blank">Junella</a>
         </li>
         <li className="nav-item">
-            <a className="nav-link" href="https://www.linkedin.com/in/karminabarajas/">Karmina</a>
+            <a className="nav-link" href="https://www.linkedin.com/in/karminabarajas/"target="_blank">Karmina</a>
         </li>
         <li className="nav-item">
-            <a className="nav-link" href="https://www.linkedin.com/in/williamacarter/">William</a>
+            <a className="nav-link" href="https://www.linkedin.com/in/williamacarter/"target="_blank">William</a>
         </li>
     </ul>
         <div className="footer-copyright text-center py-3">© 2022 packed:
