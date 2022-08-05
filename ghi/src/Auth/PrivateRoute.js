6ea20609877968283f63930e078ae37d89397a0a
jinspins <uncleben096@gmail.com>
2022-08-05 13:42:19 -0500

    added comments for PrivateRoute

--- ghi/src/Auth/PrivateRoute.js
+++ ghi/src/Auth/PrivateRoute.js
@@ -2,10 +2,12 @@ import { Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import AuthContext from '../context/AuthContext';
 
-const PrivateRoute = ({children}) => {
-    let {user} = useContext(AuthContext);
+// wrapping a route in this ensures that only authenticated
+// users can access the component
+const PrivateRoute = ({ children }) => {
+    let { user } = useContext(AuthContext);
     return (
-       !user ? <Navigate to="/login" /> : children
+        !user ? <Navigate to="/login" /> : children
     )
 }
 
